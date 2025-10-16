5d92edc7c6d79e277bc5a7cf462141448ed28fd5
chentt06 <trinacheng43@gmail.com>
2025-10-16 11:10:36 -0400

    more comments

--- game/StrongerAlien.java
+++
@@ -1,9 +0,0 @@
-package game;
-
-public class StrongerAlien extends Alien{
-    
-    public StrongerAlien(Point[] points, Point position, int rotation, int hp){
-        super(points, position, rotation, hp);
-    }
-
-}
